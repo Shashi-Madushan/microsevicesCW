3d639b747550621b20eaec46b08a37af8edf5d4f
Shashi Madushan <156106584+Shashi-Madushan@users.noreply.github.com>
2025-06-17 22:48:00 +0530

    initial setup is implemented with four services

---
+++ eureka-server/src/test/java/com/shashi/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shashi.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
